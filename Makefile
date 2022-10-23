5991133d8dfd3d0ccee52c7e3a62555e8fcd8f06
activationeuron <aforamitrai@gmail.com>
2022-10-23 13:20:16 +0530

    added migrations!

---
+++ Makefile
@@ -0,0 +1,8 @@
+SHELL:=/bin/bash
+
+version=1.0
+
+all: install
+
+install:
+  yarn install
---
+++ scripts/index.ts
@@ -0,0 +1,18 @@
+import migrate, { RunnerOption } from 'node-pg-migrate';
+import 'dotenv/config';
+
+const options: RunnerOption = {
+  direction: 'up',
+  dir: 'migrations',
+  migrationsTable: 'pgmigrations',
+  databaseUrl: process.env.DB_URL!,
+};
+console.log(process.env.DB_URL);
+
+migrate(options)
+  .then((res: any) => {
+    console.log(res);
+  })
+  .then((err: any) => {
+    console.log(err);
+  });
